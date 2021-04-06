6f8ff324dab915530be2a7701f1de1b6ad78deb4
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-04-07 01:35:46 +0300

    Основные заданий по многопоточному сервесу и доп задание №1

---
+++ scanner.py
@@ -0,0 +1,61 @@
+import asyncio
+import socket
+import shutil
+from progress.bar import IncrementalBar
+
+checked = 0
+cols = shutil.get_terminal_size().columns
+bar = IncrementalBar(max=65536)
+
+
+def checkIp(ip):
+    try:
+        sum = 0
+        if ip == 'localhost':
+            return True
+        parts = ip.split(".", 4)
+        if len(parts) == 4:
+            for part in parts:
+                part = int(part)
+                if -1 < part < 256:
+                    sum += 1
+        else:
+            return False
+        if sum != 4:
+            return False
+    except ValueError:
+        return False
+
+
+async def check(host, port, free):
+    global checked, cols, bar
+    sock = socket.socket()
+    try:
+        await sock.connect((host, port))
+    except TypeError:
+        print(f" <-- Порт {port} открыт")
+        free.append(port)
+    except OSError:
+        pass
+    finally:
+        bar.next()
+        checked += 1
+        sock.close()
+
+
+async def main(host):
+    free = []
+    t = []
+    for port_number in range(65536):
+        t.append(check(host, port_number, free))
+    await asyncio.wait(t)
+    bar.finish()
+    free = sorted(free)
+    fin = ','.join(str(i) for i in free)
+    print(f'Свободные => { fin }')
+
+host = input('Имя хоста/IP-адрес: ')
+if checkIp(host):
+    asyncio.run(main(host))
+else:
+    print("Введено неверно")
